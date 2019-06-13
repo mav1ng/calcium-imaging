94249cc2e5d362c61f498c975f2084ff8c579a88
mav1ng <m.spreng@stud.uni-heidelberg.de>
2019-06-13 17:00:55 +0200

    rewrote forward() method, added some clones() backprop working now

--- test.py
+++ test.py
@@ -20,31 +20,3 @@ import neurofinder as nf
 
 
 
-with torch.no_grad():
-    self.bs = x_in.size(0)
-    self.emb = x_in.size(1)
-    self.w = x_in.size(2)
-    self.h = x_in.size(3)
-
-
-x = x_in.view(self.bs, self.emb, -1)
-
-y = torch.zeros(self.nb_iterations + 1, self.emb, self.w * self.h)
-out = torch.zeros(self.bs, self.nb_iterations + 1, self.emb, self.w, self.h,
-                  device=self.device, dtype=self.dtype)
-
-# iterating over all samples in the batch
-for b in range(self.bs):
-    y[0, :, :] = x[b, :, :]
-    for t in range(1, self.nb_iterations):
-        y[t, :, :] = y[t - 1, :, :]
-        kernel_mat = torch.exp(torch.mul(self.kernel_bandwidth, mm(y[t, :, :].t(), y[t, :, :])))
-        diag_mat = torch.diag(
-            mm(kernel_mat.t(), torch.ones((self.w * self.h, 1), device=self.device, dtype=self.dtype)).squeeze(dim=1),
-            diagonal=0)
-
-        y[t, :, :] = mm(y[t, :, :], torch.add(torch.mul(self.step_size, mm(kernel_mat, torch.inverse(diag_mat))),
-                                              torch.mul(1. - self.step_size, torch.eye(self.w * self.h))))
-    out[b, :, :, :, :] = y.view(self.nb_iterations, self.emb, self.w, self.h)
-
-return out

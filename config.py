correcting the loss normalizing mainly it went to negative number after a while

config.py
@@ -10,7 +10,7 @@ mean_shift = dict(
     embedding_dim=20,
     kernel_bandwidth=None,                  # set to float if should be used, margin is now used to calculate bandwidth
     step_size=1.,                            # mean shift step size
-    nb_iterations=5,                       # number of iterations, if < 1 model UNet with Unit Sphere Normalization
+    nb_iterations=0,                       # number of iterations, if < 1 model UNet with Unit Sphere Normalization
 )
 
 embedding_loss = dict(
@@ -21,6 +21,7 @@ data = dict(
     different_labels=True,
     use_compression=True,
     dtype=torch.float,
+    snapshots='models/'
 )
 
 corr = dict(
@@ -30,23 +31,23 @@ corr = dict(
 
 training = dict(
     train=True,
-    lr=0.002,
+    lr=0.0001,
     nb_epochs=10,
     img_size=64,
-    batch_size=10,
+    batch_size=20,
 
     min_neuron_pixels=0.1,
 )
 
 cuda = dict(
-    use_mult=True,
+    use_mult=False,
     device=torch.device('cuda:0'),
     mult_device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
-    use_devices=[0, 1, 2, 3],
+    use_devices=[0, 1],
 )
 
 tb = dict(
-    loss_name='iter_5'
+    loss_name='iter_0'
 )
 
 debug = dict(

main.py
@@ -60,7 +60,7 @@ torch.cuda.empty_cache()
 
 transform = transforms.Compose([data.CorrRandomCrop(img_size, summary_included=True)])
 comb_dataset = data.CombinedDataset(corr_path='data/corr/starmy/sliced/slice_size_100/', sum_folder='data/sum_img/',
-                                    transform=transform, device=device, dtype=dtype)
+                                    transform=None, device=device, dtype=dtype)
 
 
 print('Loaded the Dataset')
@@ -101,6 +101,9 @@ if train:
             input = batch['image'].cuda()
             label = batch['label'].cuda()
 
+            input = comb_dataset[0]['image'][:, :64, :64].view(1, 28, 64, 64).cuda()
+            label = comb_dataset[0]['label'][:64, :64].view(1, 64, 64).cuda()
+
             input.requires_grad = True
             label.requires_grad = True
 
@@ -124,9 +127,11 @@ if train:
 
 
             loss = criterion(output, label)
-            loss = n.scaling_loss(loss, batch_size, c.cuda['use_devices'].__len__())
-
-            writer.add_scalar('Training Loss', loss.detach())
+            if c.cuda['use_mult']:
+                loss = n.scaling_loss(loss, batch_size, c.cuda['use_devices'].__len__())
+                writer.add_scalar('Training Loss', loss.detach())
+            else:
+                writer.add_scalar('Training Loss', loss.item())
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -203,19 +208,30 @@ if not train:
 #     num_samples = c.data['num_samples']
 #     nb_epochs = c.training['nb_epochs']
 #     val_freq = c.val['val_freq']
-#     snapshots = c.data['snapshots']
 #
-#     model = n.UNetMS()
 #
 #     if c.cuda['use_mult']:
-#         model = torch.nn.DataParallel(model, device_ids=[0, 1, 2, 3])
 #         device = c.cuda['mult_device']
+#     else:
+#         device = c.cuda['device']
 #
-#     model.cuda()
+#         model = n.UNetMS()
+#
+#
+#     if c.cuda['use_mult']:
+#         model = nn.DataParallel(model, device_ids=c.cuda['use_devices']).cuda()
+#     else:
+#         model.to(device)
+#     model.type(dtype)
+#
 #
 #     # define loss function (criterion) and optimizer
 #     # perhaps add additional loss functions if this one does not work
 #     emb_loss = n.EmbeddingLoss().cuda()
+#
+#     if c.cuda['use_mult']:
+#         emb_loss = nn.DataParallel(emb_loss, device_ids=c.cuda['use_devices']).cuda()
+#
 #     criterion = [emb_loss]
 #
 #     # # creating different parameter groups
@@ -242,7 +258,7 @@ if not train:
 #         best_f1 = 0.
 #
 #     # models are save only when their loss obtains the best value in the validation
-#     valtrack = 0
+#     f1track = 0
 #
 #     print 'There are %d parameter groups' % len(optimizer.param_group)
 #     print 'Initial base params lr: %f' % optimizer.param_group[0]['lr']
@@ -281,9 +297,9 @@ if not train:
 #
 #             # check patience
 #             if f1_metric <= best_f1:
-#                 valtrack += 1
+#                 f1track += 1
 #             else:
-#                 valtrack = 0
+#                 f1track = 0
 #
 #             # save the best model
 #             is_best = f1_metric < best_f1
@@ -293,11 +309,11 @@ if not train:
 #                 'state_dict': model.state_dict(),
 #                 'best_val': best_f1,
 #                 'optimizer': optimizer.state_dict(),
-#                 'valtrack': valtrack,
+#                 'valtrack': f1track,
 #                 'curr_val': f1_metric,
 #             }, is_best)
 #
-#             print '** Validation: %f (best) - %d (valtrack)' % (best_f1, valtrack)
+#             print '** Validation: %f (best) - %d (f1track)' % (best_f1, f1track)
 #
 #
 # def train(train_loader, model, criterion, optimizer, epoch):
@@ -413,6 +429,7 @@ if not train:
 #
 #
 # def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+#     snapshots = c.data['snapshots']
 #     filename = snapshots + 'model_e%03d_v-%.3f.pth.tar' % (state['epoch'], state['best_val'])
 #     if is_best:
 #         torch.save(state, filename)

network.py
@@ -284,8 +284,8 @@ def comp_similarity_matrix(input):
     out = torch.zeros((h * w, h * w, 1, bs)).cuda()
 
     for i in range(bs):
-        sim = input[i].view(h * w, ch)
-        out[:, :, 0, i] = torch.mm(sim, sim.t())
+        sim = F.normalize(F.normalize(input[i].view(ch, w * h).t(), dim=1), dim=0)
+        out[:, :, 0, i] = torch.mm(sim, sim.t()) * 0.5 + 0.5
 
     return out
 
@@ -402,9 +402,11 @@ def scaling_loss(loss_vec, bs, nb_gpus):
     """
     assert bs >= nb_gpus, 'Batch Size should be bigger than the number of working gpus'
     rem = bs % nb_gpus
-    b = (bs - rem) / float((nb_gpus - 1))
+    if rem != 0:
+        nb_gpus = nb_gpus - 1
+    b = (bs - rem) / float(nb_gpus)
     out = 0.
-    for g in range(nb_gpus - 1):
+    for g in range(nb_gpus):
         out = out + loss_vec[g] / b
     if rem != 0:
         out = out + loss_vec[-1] / rem